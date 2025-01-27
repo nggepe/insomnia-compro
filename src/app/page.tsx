d2cbb3c321185e938c51f22d14804156f6356272
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2025-01-27 14:36:02 +0700

    chore: landing page framework (#3)
    # Description
    
    - add landing page skeleton
    - add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import About from "@/components/About";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <main>
+      <Hero/>
+      <About/>
+      <Services/>
+    </main>
     </>
   );
 }
