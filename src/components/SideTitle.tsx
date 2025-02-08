5241a368a3060c9c1c6bb0ac304171ac12880cea
Agra Wimba <agrawimba@gmail.com>
2025-02-08 16:39:04 +0700

    feat(introduction-section): Add interface for SideTitle component

--- src/components/SideTitle.tsx
+++ src/components/SideTitle.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
+
+interface SideTitleProps {
+  children: ReactNode;
+  className?: string;
+}
 
 export default function SideTitle({
   children,
   className = "",
-}) {
+}: SideTitleProps) {
   return (
     <div
       className={`px-4 py-3 font-semibold bg-gradient-to-r from-[#FE8A04] via-[#FFE520] to-[#6DD347] text-[#010000] -rotate-90 text-sm md:text-lg ${className}`}
