c81b2f80db32b74ddd31788176966da0308f7faf
wimbaagra <agrawimba@gmail.com>
2025-02-11 11:52:01 +0700

    feat(services-section): Create services section (#7)
    * feat(navbar): Add navbar with menu
    
    * feat(navbar): Set navbar to responsive view
    
    * feat(hero-section): Add hero section and adjust navbar
    
    * Apply suggestions from code review
    
    Co-authored-by: Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
    
    * feat(hero-section): Add interface
    
    * feat(hero-section): fix error lint
    
    * add husky
    
    * update error menu
    
    * feat(hero-section): update height size for hero section
    
    * feat(introduction-section): Create introduction section
    
    * feat(introduction-section): Add interface for SideTitle component
    
    * feat(introduction-section): Change component name
    
    * feat(introduction-section): fix section high
    
    * feat(services-section): Create services section
    
    ---------
    
    Co-authored-by: Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,3 +1,73 @@
+import SectionContent from "./SectionContent";
+import ServiceItem from "./ServiceItem";
+import SideMark from "./SideMark";
+
+const serviceItems = [
+  {
+    key: 1,
+    title: '1. Web App Development',
+    description: 'Build a professional, high-converting website to showcase your brand',
+    imageSrc: '/service-1.png',
+    keywords: ['color', 'design', 'typography', 'wireframe']
+  },
+  {
+    key: 2,
+    title: '2. Mobile App Development',
+    description: 'Engage customers with user-friendly mobile applications.',
+    imageSrc: '/service-2.png',
+    keywords: ['color', 'design', 'typography', 'wireframe']
+  },
+  {
+    key: 3,
+    title: '3. Social Media Expert',
+    description: 'Boost your online visibility with strategic social media management.',
+    imageSrc: '/service-3.png',
+    keywords: ['color', 'design', 'typography', 'wireframe']
+  },
+  {
+    key: 4,
+    title: '4. Digital Advertising',
+    description: 'Engage customers with user-friendly mobile applications.',
+    imageSrc: '/service-4.png',
+    keywords: ['color', 'design', 'typography', 'wireframe']
+  },
+  {
+    key: 5,
+    title: '5. Business Management Digitalization',
+    description: 'Optimize processes and improve efficiency with smart digital tools.',
+    imageSrc: '/bg-services.png',
+    keywords: ['color', 'design', 'typography', 'wireframe']
+  }
+];
+
 export default function Services() {
-  return <section id="services">Services Section</section>;
+  return (
+    <section
+      id="services"
+      className="relative px-3 py-8 md:px-12 md:py-16 text-white bg-center bg-cover h-auto"
+      style={{ backgroundImage: "url('/bg-services.png')"}}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
+      <div className="absolute inset-0 h-2/3 bg-gradient-to-b from-black to-transparent" />
+      <SideMark>SERVICES</SideMark>
+      <div className="relative z-10">
+        <SectionContent
+          title="We offer several services">
+          So, how can a small local business like a plumber, gardener, or other tradesperson follow the same path as these global giants? Start by creating a strong online presence—a professional, user-friendly website is essential. Add a Google My Business listing to ensure you appear in local searches. Use platforms like Facebook and Instagram to share before-and-after photos of your work, customer testimonials, and engaging videos. Consistent, hyper-local advertising campaigns can help you target your community effectively, bringing in steady leads and building trust. Similarly, small retail businesses like coffee shops, pizzerias, and restaurants can harness digital marketing by focusing on visually appealing content. Share mouthwatering photos on Instagram, leverage TikTok trends to showcase your personality, and encourage customers to leave reviews online. Add online ordering and delivery options to your website and utilize targeted ads to drive traffic during peak hours. These simple yet impactful strategies can help you connect with more customers, grow your brand, and scale your sales beyond expectations.
+        </SectionContent>
+        <div className="flex flex-col flex-wrap md:flex-row px-4">
+          {serviceItems.map((item) => (
+            <ServiceItem
+              key={item.key}
+              title={item.title}
+              description={item.description}
+              imageSrc={item.imageSrc}
+              keywords={item.keywords}
+              className="w-full md:w-1/2"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
--- src/components/SideMark.tsx
+++ src/components/SideMark.tsx
@@ -13,7 +13,7 @@ export default function SideMark({
 }: SideMarkProps) {
   return (
     <div
-      className={`px-4 py-3 font-semibold bg-gradient-to-r from-[#FE8A04] via-[#FFE520] to-[#6DD347] text-[#010000] -rotate-90 text-sm md:text-lg ${className}`}
+      className={`px-4 py-3 font-semibold bg-gradient-to-r from-[#FE8A04] via-[#FFE520] to-[#6DD347] text-[#010000] -rotate-90 text-xs md:text-base absolute top-20 md:top-28 -left-8 md:-left-9 ${className}`}
     >
       {children}
     </div>
